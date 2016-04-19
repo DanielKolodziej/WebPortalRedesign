177415175a8396e6fc99bc6331af75a388c3e6b2
Daniel Kolodziej <dkolodz1@hawk.iit.edu>
2016-04-18 20:29:09 -0500

    put in function to identify a user and pass

--- js/log.js
+++ js/log.js
@@ -1,1 +1,19 @@
-
+function myFunction() {
+    var emailVal = document.getElementById("username").value;
+	var passVal = document.getElementById("password").value;
+    // If the username is "testuser" and pass is "testpass"
+    if (emailVal === "testuser" && passVal === "testpass") {
+        document.getElementById("username").style.background="#b2ffb2";
+		document.getElementById("password").style.background="#b2ffb2";
+    // If the username is "testuser" and pass is wrong
+    } 
+	else if(emailVal === "testuser" && passVal !== "testpass")
+	{
+		document.getElementById("username").style.background="#b2ffb2";
+		document.getElementById("password").style.background="#ff4c4c";
+	}
+	else {
+        document.getElementById("username").style.background="#ff4c4c";
+		document.getElementById("password").style.background="#ff4c4c";
+    }
+}
